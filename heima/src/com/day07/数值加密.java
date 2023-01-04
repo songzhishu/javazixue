4be7550b535bf22503f0d90a0d97703c1a2c9bd9
宋之树 <2334304096@qq.com>
2023-01-04 11:18:29 +0800

    Commit Message

--- heima/src/com/day07/数值加密.java
+++ heima/src/com/day07/数值加密.java
@@ -8,6 +8,7 @@ public class 数值加密 {
         Scanner sr = new Scanner(System.in);
         System.out.print("请输入你要加密的数值:");
         int originalCode = sr.nextInt();
+        int originalCode1 = originalCode;
         //判断他是几位数
         int digit = 1;
         for (int i = 0; i < 10; i++) {
@@ -16,51 +17,30 @@ public class 数值加密 {
                 digit++;
             }
         }
+        System.out.println(originalCode1);
         //生成对应长度的数组
         int arr[] = new int[digit];
+        /*System.out.println(arr.length);*/
         //将加密值存入数组
-
-        for (int i = 0; i < arr.length; i++) {
-            //取数
-            if (originalCode <= 9) {
-                arr[0] = originalCode;
-                //System.out.println(arr[0]);
+        for (int i = 0, j = arr.length - 1; i < arr.length; i++, j--) {
+            if (originalCode1 <= 9) {
+                arr[0] = originalCode1;
             } else {
-                for (int k = 0; k < arr.length; k++) {
-                    if (originalCode <= 9) {
-                        arr[0] = originalCode;
-                        //System.out.println(arr[0]);
-                    } else {
-                        int l = arr.length - 1;
-                        for (int j = 0; j < digit; j++) {
-                            //l是为了方便赋值
-                            if (l >= 0) {
-                                arr[l] = (originalCode % 10);
-                                l--;
-                                break;
-                            }
-                        }
-                    }
-                    originalCode = originalCode / 10;
-                }
+                arr[j] = originalCode1 % 10;
             }
+            originalCode1 = originalCode1 / 10;
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        int afterEncryption[] = encryption(arr);
+        System.out.print("[");
+        for (int i = 0; i < afterEncryption.length; i++) {
+            System.out.print(afterEncryption[i] + " ");
         }
+        System.out.print("]");
+
     }
 
 
-
-       /* int arr1[]=encryption(arr);
-        System.out.print("[");
-        for (int i = 0; i < arr1.length; i++) {
-            System.out.print(" "+arr1[i]+" ");
-        }
-        System.out.print("]");
-    }*/
-
-   /* public static int[] encryption(int x[]) {
+    public static int[] encryption(int x[]) {
         //进行加密操作
         for (int i = 0; i < x.length; i++) {
             x[i] = (x[i] + 5) % 10;
@@ -68,19 +48,26 @@ public class 数值加密 {
 
 
         //反转
-        int j = x.length-1;
+        int j = x.length - 1;
         for (int i = 0; i < x.length; i++) {
-            if(x.length!=1){
+            if (x.length != 1) {
                 if (i < j) {
-                    int temp=0;
-                    temp=x[i];
+                    int temp = 0;
+                    temp = x[i];
                     x[i] = x[j];
-                    x[j]=temp;
+                    x[j] = temp;
                     i++;
                     j--;
                 }
             }
         }
         return x;
-    }*/
+    }
+
+    /*终于改完了 草!
+     * 唯一的错误就是在得到用户输入的数据的时候
+     * 我将原始值进行了取整导致原始值改变,然后我在后续的操作中将原始值直接传递
+     * 以至于出现了当我输入一位数的时候正确,当出现多位数的时候出现错误
+     * 同时呐,我也发现了我一个不好的地方 就是喜欢嘎嘎写代码 中途不检查
+     * 循环不是太好*/
 }
